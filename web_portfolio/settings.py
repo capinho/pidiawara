4008b42a2b5d213f1d226fcb05d582b79a8f3c87
capinho <papadiawara@hotmail.fr>
2021-06-07 16:39:27 +0000

    Database conn

--- web_portfolio/settings.py
+++ web_portfolio/settings.py
@@ -91,6 +91,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
@@ -130,9 +132,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'staticfiles')
-# ]
 
 STATIC_URL = '/static/'
 
